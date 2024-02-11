93c4db3ff9ea32803ba3fbeaccf11870243950c6
officiallyjo <josie9447@gmail.com>
2024-02-11 17:00:01 +0100

    Your commit message

---
+++ host_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def hompage (request):
+    return  render (request ,"host_app/homepage.html")
